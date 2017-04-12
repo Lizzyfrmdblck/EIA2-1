a0f09a6277f3987801bcf32592a781f44020b2f4
Yannic Hermann <Yannic Hermann@DESKTOP-2B3DOTU.SpeedportEntry2090126200030>
2017-04-12 23:32:28 +0200

    Konzept 4

--- aufgabe3b-MauMau/3b.js
+++ aufgabe3b-MauMau/3b.js
@@ -1,1 +1,10 @@
+window.onload = function () {
+    var deck = ["Herz Dame", "Karo Bube", "Herz Ass"];
+    var discard = [];
+    var hand = [];
+    document.getElementById("deck").addEventListener("click", function pullCard() {
+        var randomCard = [deck[Math.floor(Math.random() * deck.length)]];
+        var pushRandomCardToDeck = randomCard.push("");
+    });
+};
 //# sourceMappingURL=3b.js.map
---
+++ aufgabe3b-MauMau/3b.ts
@@ -0,0 +1,15 @@
+window.onload = function (): void {
+
+    let deck: string[] = ["Herz Dame", "Karo Bube", "Herz Ass"];
+    let discard: string[] = [];
+    let hand: string[] = [];
+    
+    document.getElementById("deck").addEventListener("click", function pullCard(): void {
+        
+        let randomCard: string [] = [deck[Math.floor(Math.random() * deck.length)]];
+        let pushRandomCardToHand: string = randomCard.push("");
+
+       pushRandomCardToHand.concat.document.getElementBy
+        
+        });
+    };
