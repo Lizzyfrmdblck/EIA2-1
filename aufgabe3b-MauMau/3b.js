e877bb41e9605ac4733bc0b2e86e88f9d7a3e0f7
Yannic Hermann <Yannic Hermann@DESKTOP-2B3DOTU.SpeedportEntry2090126200030>
2017-04-20 10:56:58 +0200

    3b try

--- aufgabe3b-MauMau/3b.js
+++ aufgabe3b-MauMau/3b.js
@@ -1,5 +1,5 @@
 window.onload = function () {
-    var deck = ["Herz 7", "Herz Dame", "Karo Bube", "Karo Dame", "Herz K�nig"];
+    var deck = ["Herz 7", "Herz 8", "Herz 9", "Herz 10", "Herz Bube", "Herz Dame", "Herz K�nig", "Herz Ass", "Pik 7", "Pik 8", "Pik 9", "Pik 10", "Pik Bube", "Pik Dame", "Pik K�nig", "Pik Ass", "Karo 7", "Karo 8", "Karo 9", "Karo 10", "Karo Bube", "Karo Dame", "Karo K�nig", "Karo Ass", "Kreuz 7", "Kreuz 8", "Kreuz 9", "Kreuz 10", "Kreuz Bube", "Kreuz Dame", "Kreuz K�nig", "Kreuz Ass"];
     var discard = [];
     var hand = [];
     var getDeck = document.getElementById("deck");
@@ -8,14 +8,27 @@ window.onload = function () {
     //addClick
     getDeck.addEventListener("click", pullCard);
     function pullCard() {
-        //createDiv
-        var createDiv = document.createElement("div");
-        //addToHand
-        document.getElementById("handcards").appendChild(createDiv);
-        //randomCard
-        var randomCard = Math.floor((Math.random() * 3));
-        //divContent = zuf�lliges
-        createDiv.textContent = deck[randomCard];
+        if (hand.length < 5 && deck.length > 0) {
+            //randomCard
+            var randomCard = Math.floor((Math.random() * 32 - 1));
+            //createDiv
+            var createDiv = document.createElement("div");
+            //addToHand
+            document.getElementById("handcards").appendChild(createDiv);
+            //divContent = zuf�lliger IndexOfArray
+            createDiv.textContent = deck[randomCard];
+        }
+        getHand.addEventListener("click", pushCardToDiscard);
+        function pushCardToDiscard() {
+            for (var i = 0; i < hand.length; i++) {
+                hand.splice(i, 1);
+                discard.push(hand[i]);
+                break;
+            }
+            var createDiv = document.createElement("div");
+            document.getElementById("discard").textContent = createDiv.textContent + deck.length;
+            createDiv.parentNode.removeChild(createDiv);
+        }
     }
 };
 //# sourceMappingURL=3b.js.map
--- aufgabe3b-MauMau/3b.ts
+++ aufgabe3b-MauMau/3b.ts
@@ -1,6 +1,6 @@
 window.onload = function (): void {
     
-    let deck: string[] = ["Herz 7", "Herz Dame", "Karo Bube", "Karo Dame", "Herz König"];
+    let deck: string[] = ["Herz 7", "Herz 8", "Herz 9", "Herz 10", "Herz Bube", "Herz Dame", "Herz König", "Herz Ass", "Pik 7", "Pik 8", "Pik 9", "Pik 10", "Pik Bube", "Pik Dame", "Pik König", "Pik Ass", "Karo 7", "Karo 8", "Karo 9", "Karo 10", "Karo Bube", "Karo Dame", "Karo König", "Karo Ass", "Kreuz 7", "Kreuz 8", "Kreuz 9", "Kreuz 10", "Kreuz Bube", "Kreuz Dame", "Kreuz König", "Kreuz Ass"];
     let discard: string[] = [] ;
     let hand: string[] = [] ;
     
@@ -13,15 +13,31 @@ window.onload = function (): void {
     
     function pullCard(): void {
         
+        if (hand.length < 5 && deck.length > 0) {
+        //randomCard
+        let randomCard: number = Math.floor((Math.random() * 32 - 1));
+
         //createDiv
         let createDiv: HTMLDivElement = document.createElement("div");
         //addToHand
-        document.getElementById("handcards").appendChild(createDiv);
-        //randomCard
-        let randomCard: number = Math.floor((Math.random() * 3));
-        
-        //divContent = zufälliges
-        createDiv.textContent = deck[randomCard];
-    }
+        document.getElementById("handcards").appendChild(createDiv);        
+        //divContent = zufälliger IndexOfArray
+        createDiv.textContent = deck[randomCard]; 
             
-    };
+        }
+        
+    getHand.addEventListener("click", pushCardToDiscard);
+        
+        function pushCardToDiscard(): void {
+            
+        for (var i: number = 0; i < hand.length; i++) {
+            hand.splice(i, 1);
+            discard.push(hand[i]);
+            break;
+        }
+        let createDiv: HTMLDivElement = document.createElement("div");
+        document.getElementById("discard").textContent = createDiv.textContent + deck.length;
+        createDiv.parentNode.removeChild(createDiv);
+}
+    }        
+};
