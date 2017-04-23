e324ed24037260efe5afedccff306234db4ea89d
Yannic Hermann <Yannic Hermann@DESKTOP-2B3DOTU.SpeedportEntry2090126200030>
2017-04-23 10:02:41 +0200

    random flowers

--- aufgabe4-Blumenwiese/blumenwiese.js
+++ aufgabe4-Blumenwiese/blumenwiese.js
@@ -17,25 +17,25 @@ var L4_Canvas;
         crc2.fillStyle = colorGradient;
         crc2.fillRect(0, -225, canvas.width, canvas.height);
         //MOUNTAIN
-        drawmountain(50, 100, 90, "#000000", "#7F7F7F");
-        drawmountain(150, 150, 50, "#000000", "#878787");
-        drawmountain(620, 150, 200, "#000000", "#9E9E9E");
-        drawmountain(500, 200, 175, "#000000", "#ABABAB");
+        drawMountain(50, 100, 90, "#000000", "#7F7F7F");
+        drawMountain(150, 150, 50, "#000000", "#878787");
+        drawMountain(620, 150, 200, "#000000", "#9E9E9E");
+        drawMountain(500, 200, 175, "#000000", "#ABABAB");
         //SUN
-        drawsun(0, 0, "#ffff00", "#FFD700");
+        drawSun(0, 0, "#ffff00", "#FFD700");
         //FIELD        
         crc2.fillStyle = "#68b300";
         crc2.fillRect(0, 175, canvas.width, canvas.height);
         //STREET
-        drawstreet(225, 400, "#000000", "#404040");
+        drawStreet(225, 400, "#000000", "#404040");
         //STRIPES
-        drawstripes(300, 390, 15, 360, "#ffffff", "#ffffff");
-        drawstripes(300, 340, 12, 315, "#ffffff", "#ffffff");
-        drawstripes(300, 300, 9, 280, "#ffffff", "#ffffff");
-        drawstripes(300, 265, 7, 250, "#ffffff", "#ffffff");
-        drawstripes(300, 235, 5, 220, "#ffffff", "#ffffff");
-        drawstripes(300, 210, 3, 200, "#ffffff", "#ffffff");
-        drawstripes(300, 190, 2, 180, "#ffffff", "#ffffff");
+        drawStripes(300, 390, 15, 360, "#ffffff", "#ffffff");
+        drawStripes(300, 340, 12, 315, "#ffffff", "#ffffff");
+        drawStripes(300, 300, 9, 280, "#ffffff", "#ffffff");
+        drawStripes(300, 265, 7, 250, "#ffffff", "#ffffff");
+        drawStripes(300, 235, 5, 220, "#ffffff", "#ffffff");
+        drawStripes(300, 210, 3, 200, "#ffffff", "#ffffff");
+        drawStripes(300, 190, 2, 180, "#ffffff", "#ffffff");
         //CLOUD3
         drawCloud(180, 75, 10, "#000000", "#ffffff");
         drawCloud(170, 70, 10, "#000000", "#ffffff");
@@ -57,16 +57,50 @@ var L4_Canvas;
         //BIRDS
         drawBird(320, 50, 338, 58, "#000000");
         drawBird(305, 65, 323, 73, "#000000");
-        //RANDOMFLOWERS
-        //        function getRandomArbitrary(min: number = 1, max: number = 250) {
-        //        let random: number = Math.floor(Math.random() * (max - min)) + min;
-        //        console.log(random);
-        //            let randomCard: number = Math.floor((Math.random() * 3));
-        //        drawRandomFlower(randomCard, 100);
-        //        }
+        //RANDOMFLOWERS LEFT
+        var colorBucket = ["#F7FE2E", "#2E2EFE", "#FE9A2E", "#FA58F4", "#81DAF5"];
+        var colorBucket1 = ["#81BEF7", "#F781BE", "#DF0101", "#81F7D8", "#F5DA81"];
+        var centerColors = ["#b879fc", "#30e3f4", "#f4f130"];
+        for (var i = 0; i < 35; i++) {
+            var randomX = (Math.random() * (215 - 15) + 15);
+            var randomY = (Math.random() * (375 - 200) + 200);
+            var randomCenterSize = (Math.random() * (7.5 - 4) + 4);
+            var randomLeaveSize = (Math.random() * (8 - 4) + 4);
+            var randomColor = colorBucket[Math.floor(Math.random() * colorBucket.length)];
+            var randomColor1 = colorBucket1[Math.floor(Math.random() * colorBucket1.length)];
+            var randomCenterColor = centerColors[Math.floor(Math.random() * centerColors.length)];
+            var randomFlower = Math.floor((Math.random() * 2)) + 1;
+            if (randomFlower == 1) {
+                drawFlower0(randomX, randomY, randomCenterSize, randomLeaveSize, randomCenterColor, randomColor);
+            }
+            else {
+                drawFlower0(randomX, randomY, randomCenterSize, randomLeaveSize, randomCenterColor, randomColor1);
+            }
+        }
+        //RANDOMFLOWERS RIGTH
+        //        let colorBucket: string[] = ["#F7FE2E", "#2E2EFE", "#FE9A2E", "#FA58F4", "#81DAF5"];
+        //        let colorBucket1: string[] = ["#81BEF7", "#F781BE", "#DF0101", "#81F7D8", "#F5DA81"];
+        //        let centerColors: string[] = ["#b879fc", "#30e3f4", "#f4f130"];
+        //                
+        for (var i = 0; i < 35; i++) {
+            var randomX = (Math.random() * (565 - 385) + 385);
+            var randomY = (Math.random() * (375 - 200) + 200);
+            var randomCenterSize = (Math.random() * (7.5 - 4) + 4);
+            var randomLeaveSize = (Math.random() * (8 - 4) + 4);
+            var randomColor = colorBucket[Math.floor(Math.random() * colorBucket.length)];
+            var randomColor1 = colorBucket1[Math.floor(Math.random() * colorBucket1.length)];
+            var randomCenterColor = centerColors[Math.floor(Math.random() * centerColors.length)];
+            var randomFlower = Math.floor((Math.random() * 2)) + 1;
+            if (randomFlower == 1) {
+                drawFlower0(randomX, randomY, randomCenterSize, randomLeaveSize, randomCenterColor, randomColor);
+            }
+            else {
+                drawFlower0(randomX, randomY, randomCenterSize, randomLeaveSize, randomCenterColor, randomColor1);
+            }
+        }
     }
     //MOUNTAIN
-    function drawmountain(_x, _y, _z, _strokeColor, _fillColor) {
+    function drawMountain(_x, _y, _z, _strokeColor, _fillColor) {
         crc2.beginPath();
         crc2.fillStyle = _fillColor;
         crc2.strokeStyle = _strokeColor;
@@ -78,7 +112,7 @@ var L4_Canvas;
         crc2.stroke();
     }
     //SUN
-    function drawsun(_x, _y, _strokeColor, _fillColor) {
+    function drawSun(_x, _y, _strokeColor, _fillColor) {
         crc2.beginPath();
         crc2.fillStyle = _fillColor;
         crc2.arc(300, 175, 80, 0, 2 * Math.PI);
@@ -87,7 +121,7 @@ var L4_Canvas;
         crc2.fill();
     }
     //STREET
-    function drawstreet(_x, _y, _strokeColor, _fillColor) {
+    function drawStreet(_x, _y, _strokeColor, _fillColor) {
         crc2.beginPath();
         crc2.fillStyle = _fillColor;
         crc2.strokeStyle = _strokeColor;
@@ -100,7 +134,7 @@ var L4_Canvas;
         crc2.stroke();
     }
     //STRIPES
-    function drawstripes(_x, _y, _width, _length, _strokeColor, _fillColor) {
+    function drawStripes(_x, _y, _width, _length, _strokeColor, _fillColor) {
         crc2.beginPath();
         crc2.fillStyle = _fillColor;
         crc2.lineWidth = _width;
@@ -129,6 +163,28 @@ var L4_Canvas;
         crc2.scale(1, 1);
         crc2.stroke();
         crc2.closePath();
+    }
+    //FLOWER0
+    function drawFlower0(_x, _y, _centerSize, _leaveSize, _centerColor, _leaveColor) {
+        //LEAVES
+        crc2.fillStyle = _leaveColor;
+        crc2.beginPath();
+        crc2.arc(_x, _y - 5, 7, 0, 2 * Math.PI);
+        crc2.fill();
+        crc2.beginPath();
+        crc2.arc(_x - 5, _y, 7, 0, 2 * Math.PI);
+        crc2.fill();
+        crc2.beginPath();
+        crc2.arc(_x + 5, _y, 7, 0, 2 * Math.PI);
+        crc2.fill();
+        crc2.beginPath();
+        crc2.arc(_x, _y + 5, 7, 0, 2 * Math.PI);
+        crc2.fill();
+        //CENTER
+        crc2.beginPath();
+        crc2.arc(_x, _y, _centerSize, 0, 2 * Math.PI);
+        crc2.fillStyle = _centerColor;
+        crc2.fill();
     }
 })(L4_Canvas || (L4_Canvas = {}));
 //# sourceMappingURL=blumenwiese.js.map
--- aufgabe4-Blumenwiese/blumenwiese.ts
+++ aufgabe4-Blumenwiese/blumenwiese.ts
@@ -18,25 +18,25 @@ namespace L4_Canvas {
         crc2.fillStyle = colorGradient;
         crc2.fillRect(0, -225, canvas.width, canvas.height);
         //MOUNTAIN
-        drawmountain(50, 100, 90, "#000000", "#7F7F7F");
-        drawmountain(150, 150, 50, "#000000", "#878787");
-        drawmountain(620, 150, 200, "#000000", "#9E9E9E");
-        drawmountain(500, 200, 175, "#000000", "#ABABAB");
+        drawMountain(50, 100, 90, "#000000", "#7F7F7F");
+        drawMountain(150, 150, 50, "#000000", "#878787");
+        drawMountain(620, 150, 200, "#000000", "#9E9E9E");
+        drawMountain(500, 200, 175, "#000000", "#ABABAB");
         //SUN
-        drawsun(0, 0, "#ffff00", "#FFD700");
+        drawSun(0, 0, "#ffff00", "#FFD700");
         //FIELD        
         crc2.fillStyle = "#68b300";
         crc2.fillRect(0, 175, canvas.width, canvas.height);
         //STREET
-        drawstreet(225, 400, "#000000", "#404040");
+        drawStreet(225, 400, "#000000", "#404040");
         //STRIPES
-        drawstripes(300, 390, 15, 360, "#ffffff", "#ffffff");
-        drawstripes(300, 340, 12, 315, "#ffffff", "#ffffff");
-        drawstripes(300, 300, 9, 280, "#ffffff", "#ffffff");
-        drawstripes(300, 265, 7, 250, "#ffffff", "#ffffff");
-        drawstripes(300, 235, 5, 220, "#ffffff", "#ffffff");
-        drawstripes(300, 210, 3, 200, "#ffffff", "#ffffff");
-        drawstripes(300, 190, 2, 180, "#ffffff", "#ffffff");
+        drawStripes(300, 390, 15, 360, "#ffffff", "#ffffff");
+        drawStripes(300, 340, 12, 315, "#ffffff", "#ffffff");
+        drawStripes(300, 300, 9, 280, "#ffffff", "#ffffff");
+        drawStripes(300, 265, 7, 250, "#ffffff", "#ffffff");
+        drawStripes(300, 235, 5, 220, "#ffffff", "#ffffff");
+        drawStripes(300, 210, 3, 200, "#ffffff", "#ffffff");
+        drawStripes(300, 190, 2, 180, "#ffffff", "#ffffff");
         //CLOUD3
         drawCloud(180, 75, 10, "#000000", "#ffffff");    
         drawCloud(170, 70, 10, "#000000", "#ffffff");
@@ -59,16 +59,54 @@ namespace L4_Canvas {
         drawBird(320, 50, 338, 58, "#000000");
         drawBird(305, 65, 323, 73, "#000000");
         
-        //RANDOMFLOWERS
-//        function getRandomArbitrary(min: number = 1, max: number = 250) {
-//        let random: number = Math.floor(Math.random() * (max - min)) + min;
-//        console.log(random);
-//            let randomCard: number = Math.floor((Math.random() * 3));
-//        drawRandomFlower(randomCard, 100);
-//        }
+        //RANDOMFLOWERS LEFT
+        let colorBucket: string[] = ["#F7FE2E", "#2E2EFE", "#FE9A2E", "#FA58F4", "#81DAF5"];
+        let colorBucket1: string[] = ["#81BEF7", "#F781BE", "#DF0101", "#81F7D8", "#F5DA81"];
+        let centerColors: string[] = ["#b879fc", "#30e3f4", "#f4f130"];
+        
+        for (let i: number = 0; i < 35; i++) {
+            let randomX: number = (Math.random() * (215 - 15) + 15);    
+            let randomY: number = (Math.random() * (375 - 200) + 200);
+            let randomCenterSize: number = (Math.random() * (7.5 - 4) + 4);            
+            let randomLeaveSize: number = (Math.random() * (8 - 4) + 4);
+            let randomColor: string = colorBucket[Math.floor(Math.random() * colorBucket.length)];
+            let randomColor1: string = colorBucket1[Math.floor(Math.random() * colorBucket1.length)];
+            let randomCenterColor: string = centerColors[Math.floor(Math.random() * centerColors.length)];
+            let randomFlower: number = Math.floor((Math.random() * 2)) + 1;
+            
+            if ( randomFlower == 1) {
+                drawFlower0(randomX, randomY, randomCenterSize, randomLeaveSize, randomCenterColor, randomColor);
+                }
+            else {
+                drawFlower0(randomX, randomY, randomCenterSize, randomLeaveSize, randomCenterColor, randomColor1);
+            }
+        } 
+        
+        //RANDOMFLOWERS RIGTH
+//        let colorBucket: string[] = ["#F7FE2E", "#2E2EFE", "#FE9A2E", "#FA58F4", "#81DAF5"];
+//        let colorBucket1: string[] = ["#81BEF7", "#F781BE", "#DF0101", "#81F7D8", "#F5DA81"];
+//        let centerColors: string[] = ["#b879fc", "#30e3f4", "#f4f130"];
+//                
+        for (let i: number = 0; i < 35; i++) {
+            let randomX: number = (Math.random() * (565 - 385) + 385);    
+            let randomY: number = (Math.random() * (375 - 200) + 200);
+            let randomCenterSize: number = (Math.random() * (7.5 - 4) + 4);            
+            let randomLeaveSize: number = (Math.random() * (8 - 4) + 4);
+            let randomColor: string = colorBucket[Math.floor(Math.random() * colorBucket.length)];
+            let randomColor1: string = colorBucket1[Math.floor(Math.random() * colorBucket1.length)];
+            let randomCenterColor: string = centerColors[Math.floor(Math.random() * centerColors.length)];
+            let randomFlower: number = Math.floor((Math.random() * 2)) + 1;
+            
+            if ( randomFlower == 1) {
+                drawFlower0(randomX, randomY, randomCenterSize, randomLeaveSize, randomCenterColor, randomColor);
+                }
+            else {
+                drawFlower0(randomX, randomY, randomCenterSize, randomLeaveSize, randomCenterColor, randomColor1);
+            }
+        }
     }
     //MOUNTAIN
-    function drawmountain(_x: number, _y: number, _z: number, _strokeColor: string, _fillColor: string): void {
+    function drawMountain(_x: number, _y: number, _z: number, _strokeColor: string, _fillColor: string): void {
         crc2.beginPath();
         crc2.fillStyle = _fillColor;
         crc2.strokeStyle = _strokeColor;
@@ -80,7 +118,7 @@ namespace L4_Canvas {
         crc2.stroke();
     }
     //SUN
-    function drawsun(_x: number, _y: number, _strokeColor: string, _fillColor: string): void {        
+    function drawSun(_x: number, _y: number, _strokeColor: string, _fillColor: string): void {        
         crc2.beginPath();
         crc2.fillStyle = _fillColor;
         crc2.arc(300, 175, 80, 0, 2 * Math.PI);
@@ -89,7 +127,7 @@ namespace L4_Canvas {
         crc2.fill();          
     }   
     //STREET
-    function drawstreet(_x: number, _y: number, _strokeColor: string, _fillColor: string): void {
+    function drawStreet(_x: number, _y: number, _strokeColor: string, _fillColor: string): void {
         crc2.beginPath();
         crc2.fillStyle = _fillColor; 
         crc2.strokeStyle = _strokeColor;
@@ -102,7 +140,7 @@ namespace L4_Canvas {
         crc2.stroke();     
     }
     //STRIPES
-    function drawstripes(_x: number, _y: number, _width: number, _length: number, _strokeColor: string, _fillColor: string): void {
+    function drawStripes(_x: number, _y: number, _width: number, _length: number, _strokeColor: string, _fillColor: string): void {
         crc2.beginPath();
         crc2.fillStyle = _fillColor; 
         crc2.lineWidth = _width;
@@ -132,13 +170,29 @@ namespace L4_Canvas {
         crc2.stroke();
         crc2.closePath();
     } 
-    //RANDOMFLOWER     
-//    function drawRandomFlower(_x: number = 0, _y: number): void {
-//        crc2.beginPath();
-//        crc2.strokeStyle = "#ffffff";
-//        crc2.moveTo(_x, _y);
-//        crc2.lineTo(_x, _y + 20);
-//        crc2.stroke();
-//        crc2.closePath();
-//    }
+    //FLOWER0
+    function drawFlower0(_x: number, _y: number, _centerSize: number, _leaveSize: number, _centerColor: string, _leaveColor: string): void {
+        //LEAVES
+        crc2.fillStyle = _leaveColor;
+        crc2.beginPath();   
+        crc2.arc(_x, _y - 5, 7, 0, 2 * Math.PI);
+        crc2.fill();
+
+        crc2.beginPath();
+        crc2.arc(_x - 5, _y, 7, 0, 2 * Math.PI);
+        crc2.fill();
+
+        crc2.beginPath();
+        crc2.arc(_x + 5, _y, 7, 0, 2 * Math.PI);
+        crc2.fill();
+
+        crc2.beginPath();
+        crc2.arc(_x, _y + 5, 7, 0, 2 * Math.PI);
+        crc2.fill();
+        //CENTER
+        crc2.beginPath();
+        crc2.arc(_x, _y, _centerSize, 0, 2 * Math.PI);
+        crc2.fillStyle = _centerColor;
+        crc2.fill();
+    }
 }
