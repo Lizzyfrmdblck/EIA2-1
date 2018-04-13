66dc8388299c36fa95d2dbc4abfd6b852f9a5ab7
Yannic Hermann <Yannic Hermann@DESKTOP-2B3DOTU.fritz.box>
2018-04-13 08:36:13 +0200

    NaN-Test not working / comment added

--- SS18/Aufgabe_2/memory.js
+++ SS18/Aufgabe_2/memory.js
@@ -1,33 +1,46 @@
 var Aufgabe_2;
 (function (Aufgabe_2) {
     window.addEventListener("load", init);
+    var n;
+    var cardContent = ["A", "B", "C", "D"];
     var askPairs;
     var numPairs = parseInt(askPairs);
+    var askPlayers;
+    var numPlayers = parseInt(askPlayers);
+    /*Asking how many Pairs + Check if it's in range + if it's a number*/
     do {
         askPairs = prompt("Wie viele Paare?");
         numPairs = parseInt(askPairs);
     } while (numPairs < 1 || numPairs > 20);
-    var askPlayers;
-    var numPlayers = parseInt(askPlayers);
+    do {
+        askPairs = prompt("Wie viele Paare?");
+        numPairs = parseInt(askPairs);
+    } while (isNaN(numPairs) || isNaN(numPairs));
+    /* Gebe ich zuerst eine falsche Nummer oder einen String ein funktionieren beide Abfragen.
+       Nach der String-Test kann ich aber auch Zahlen au�erhalb der Range eingeben und er springt weiter.
+       Ich bin mir ziemlich sicher, dass es daran liegt, dass die Abfragen in 2 versch. Schleifen stattfinden.
+       Wenn ich beide Abfragen in eine Schleife gepackt habe, habe ich es aber nicht zum Laufen gebracht.*/
+    /*Asking how many Players + Check if it's in range + if it's a number*/
     do {
         askPlayers = prompt("Wie viele Spieler?");
         numPlayers = parseInt(askPlayers);
-    } while (numPlayers < 1 || numPlayers > 4);
+    } while (numPlayers < 1 || numPlayers > 20);
+    do {
+        askPlayers = prompt("Wie viele Spieler?");
+        numPlayers = parseInt(askPlayers);
+    } while (isNaN(numPlayers) || isNaN(numPlayers));
     console.log(askPlayers);
     console.log(askPairs);
-    var n;
-    var cardContent = ["A", "B", "C", "D"];
     function init(_event) {
-        var div = document.createElement("div");
-        for (var i = 0; i < numPairs * 2; i++) {
-            document.body.appendChild(div);
-            div.innerText = "CardArray";
+        var h = document.createElement("h1");
+        h.innerText = "Dies ist eine generierte Überschrift";
+        console.log(document);
+        document.body.appendChild(h);
+        var fieldsets = document.getElementsByTagName("fieldset");
+        for (var i = 0; i < fieldsets.length; i++) {
+            console.log(fieldsets[i]);
+            console.log(fieldsets[i].textContent);
         }
-        /* let fieldsets: NodeListOf<HTMLFieldSetElement> = document.getElementsByTagName("fieldset");
-         for (let i: number = 0; i < fieldsets.length; i++) {
-             console.log(fieldsets[i]);
-             console.log(fieldsets[i].textContent);
-         }*/
     }
 })(Aufgabe_2 || (Aufgabe_2 = {}));
 //# sourceMappingURL=memory.js.map
--- SS18/Aufgabe_2/memory.ts
+++ SS18/Aufgabe_2/memory.ts
@@ -1,48 +1,62 @@
 namespace Aufgabe_2 {
     window.addEventListener("load", init);
      
-
+    let n: number; 
+    let cardContent: string[] = ["A", "B", "C", "D"];
     let askPairs: string;
-    
-    let numPairs: number = parseInt(askPairs);
-    
-    do  {
-    askPairs = prompt ("Wie viele Paare?");
-    numPairs = parseInt(askPairs);
-    } while ( numPairs < 1 || numPairs > 20); 
-
-   
+    let numPairs: number = parseInt(askPairs);  
     let askPlayers: string; 
     let numPlayers: number = parseInt(askPlayers); 
     
+/*Asking how many Pairs + Check if it's in range + if it's a number*/
     do  {
-        askPlayers = prompt ("Wie viele Spieler?");
-        numPlayers = parseInt(askPlayers);
-    } while (numPlayers < 1 || numPlayers > 4);
+    askPairs = prompt ("Wie viele Paare?");
+    numPairs = parseInt(askPairs);
+    
+    } while (numPairs < 1 || numPairs > 20); 
+     
+    do  {
+    askPairs = prompt ("Wie viele Paare?");
+    numPairs = parseInt(askPairs);
+    
+    } while (isNaN(numPairs) || isNaN(numPairs));
+
+/* Gebe ich zuerst eine falsche Nummer oder einen String ein funktionieren beide Abfragen. 
+   Nach der String-Test kann ich aber auch Zahlen auﬂerhalb der Range eingeben und er springt weiter. 
+   Ich bin mir ziemlich sicher, dass es daran liegt, dass die Abfragen in 2 versch. Schleifen stattfinden.
+   Wenn ich beide Abfragen in eine Schleife gepackt habe, habe ich es aber nicht zum Laufen gebracht.*/
+
+   
+/*Asking how many Players + Check if it's in range + if it's a number*/
+    do  {
+    askPlayers = prompt ("Wie viele Spieler?");
+    numPlayers = parseInt(askPlayers);
+    
+    } while (numPlayers < 1 || numPlayers > 20); 
+     
+    do  {
+    askPlayers = prompt ("Wie viele Spieler?");
+    numPlayers = parseInt(askPlayers);
+    
+    } while (isNaN(numPlayers) || isNaN(numPlayers)); 
+    
     
     
     console.log(askPlayers);
-    
-
-    
     console.log(askPairs);
        
-    let n: number; 
-    let cardContent: string[] = ["A", "B", "C", "D"];
-    
-    
-       
-        function init(_event: Event): void {
 
-        let div: HTMLDivElement = document.createElement("div");
-        for (let i: number = 0; i < numPairs *2; i++) {            
-            document.body.appendChild(div);
-            div.innerText = "CardArray";
-        }
-       /* let fieldsets: NodeListOf<HTMLFieldSetElement> = document.getElementsByTagName("fieldset");
+        function init(_event: Event): void {
+        
+        let h: HTMLHeadingElement = document.createElement("h1");
+        h.innerText = "Dies ist eine generierte √úberschrift";
+        console.log(document);
+        document.body.appendChild(h);
+
+        let fieldsets: NodeListOf<HTMLFieldSetElement> = document.getElementsByTagName("fieldset");
         for (let i: number = 0; i < fieldsets.length; i++) {
             console.log(fieldsets[i]);
             console.log(fieldsets[i].textContent);
-        }*/
+        }
     }
 }
