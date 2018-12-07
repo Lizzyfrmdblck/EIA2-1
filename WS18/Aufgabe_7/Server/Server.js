03f78bf574e4e255b840cc9db45598ff8482aefc
Yannic Hermann <Yannic Hermann@DESKTOP-2B3DOTU.SpeedportEntry2090126250024>
2018-12-07 10:41:08 +0100

    new server file created

--- WS18/Aufgabe_7/Server/Server.js
+++ WS18/Aufgabe_7/Server/Server.js
@@ -1,13 +1,11 @@
 "use strict";
 const Http = require("http");
-const Url = require("url");
-var WKB_Heroku;
-(function (WKB_Heroku) {
+var A7;
+(function (A7) {
     console.log("Starting server");
     let port = process.env.PORT;
     if (port == undefined)
         port = 8200;
-    let order = {};
     let server = Http.createServer();
     server.addListener("request", handleRequest);
     server.addListener("listening", handleListen);
@@ -19,11 +17,9 @@ var WKB_Heroku;
         console.log("I hear voices now!");
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
+        _response.write(_request.url);
         console.log(_request.url);
-        let url = Url.parse(_request.url, true);
-        for (let key in url.query)
-            _response.write(key + ":" + url.query[key] + "<br/>");
         _response.end();
     }
-})(WKB_Heroku || (WKB_Heroku = {}));
+})(A7 || (A7 = {}));
 //# sourceMappingURL=Server.js.map
--- WS18/Aufgabe_7/Server/Server.ts
+++ WS18/Aufgabe_7/Server/Server.ts
@@ -1,17 +1,12 @@
 import * as Http from "http";
-import * as Url from "url";
 
-namespace WKB_Heroku {
-
+namespace A7 {
     console.log("Starting server");
+    
     let port: number = process.env.PORT;
     if (port == undefined)
         port = 8200;
-
-    interface SendData { [key: string]: string; }
-
-    let order: SendData = {};
-
+    
     let server: Http.Server = Http.createServer();
     server.addListener("request", handleRequest);
     server.addListener("listening", handleListen);
@@ -20,23 +15,13 @@ namespace WKB_Heroku {
     function handleListen(): void {
         console.log("Listening");
     }
-
-
+    
     function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
-
         console.log("I hear voices now!");
-
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
-
+        _response.write(_request.url);
         console.log(_request.url);
-
-        let url: Url.Url = Url.parse(_request.url, true);
-        for (let key in url.query)
-            _response.write(key + ":" + url.query[key] + "<br/>");
-
-
         _response.end();
     }
-
 }
